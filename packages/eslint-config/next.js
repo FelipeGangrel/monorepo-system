120a8f664c2e4891e1999855b39c2021295bca57
Felipe Gangrel <felipefrog@gmail.com>
2024-12-09 08:30:21 -0300

    chore: add next config to eslint-config package

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -4,21 +4,30 @@ import tseslint from "typescript-eslint";
 import pluginReactHooks from "eslint-plugin-react-hooks";
 import pluginReact from "eslint-plugin-react";
 import globals from "globals";
+import pluginNext from "@next/eslint-plugin-next";
 import { baseConfig } from "./base.js";
 
-export const reactConfig = [
+export const nextConfig = [
   ...baseConfig,
   js.configs.recommended,
   eslintConfigPrettier,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
   {
+    ...pluginReact.configs.flat.recommended,
     languageOptions: {
       ...pluginReact.configs.flat.recommended.languageOptions,
       globals: {
         ...globals.serviceworker,
-        ...globals.browser,
       },
+    },
+  },
+  {
+    plugins: {
+      "@next/next": pluginNext,
+    },
+    rules: {
+      ...pluginNext.configs.recommended.rules,
+      ...pluginNext.configs["core-web-vitals"].rules,
     },
   },
   {
