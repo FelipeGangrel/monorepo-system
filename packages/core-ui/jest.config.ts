fd9eb020c9387057866a3a28201c451c9590ef1d
Felipe Gangrel <felipefrog@gmail.com>
2024-12-09 11:52:30 -0300

    test: add initial Jest setup for core UI components

---
+++ packages/core-ui/jest.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  roots: ["<rootDir>/src"],
+  modulePaths: ["<rootDir>/src"],
+  testEnvironment: "jsdom",
+  resetMocks: true,
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx}",
+    "!src/**/*.d.ts",
+    "!src/**/*.stories.{ts,tsx}",
+    "!src/**/*.mock.{ts,tsx}"
+  ],
+  coveragePathIgnorePatterns: [],
+  setupFilesAfterEnv: ["./jest.setup.ts"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "@swc/jest",
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+};
+
+export default config;
