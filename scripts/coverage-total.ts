bb72b24a1e753b8cd0334a5b4ca07ec77b10ea80
Felipe Gangrel <felipefrog@gmail.com>
2025-01-13 15:40:20 -0300

    formats code base

--- scripts/coverage-total.ts
+++ scripts/coverage-total.ts
@@ -9,7 +9,7 @@ type CoverageMetric = {
   covered: number;
   skipped: number;
   pct: number;
-}
+};
 
 type CoverageSummary = {
   lines: CoverageMetric;
@@ -17,12 +17,12 @@ type CoverageSummary = {
   functions: CoverageMetric;
   branches: CoverageMetric;
   [key: string]: CoverageMetric; // Allow for possible future keys.
-}
+};
 
 type ConsolidatedCoverageReport = {
   total: Partial<CoverageSummary>;
   [packageName: string]: Partial<CoverageSummary> | undefined;
-}
+};
 
 // Function 1: Get all paths for the coverage summary files
 function getAllPathsForPackagesSummaries(): Record<string, string> {
@@ -35,22 +35,38 @@ function getAllPathsForPackagesSummaries(): Record<string, string> {
   const appsPath = path.join(__dirname, '..', 'apps');
   const appsNames = getDirectories(appsPath);
 
-  const appsSummaries = appsNames.reduce((summary, appName) => {
-    return {
-      ...summary,
-      [appName]: path.join(appsPath, appName, 'coverage', 'coverage-summary.json'),
-    };
-  }, {} as Record<string, string>);
+  const appsSummaries = appsNames.reduce(
+    (summary, appName) => {
+      return {
+        ...summary,
+        [appName]: path.join(
+          appsPath,
+          appName,
+          'coverage',
+          'coverage-summary.json'
+        ),
+      };
+    },
+    {} as Record<string, string>
+  );
 
   const packagesPath = path.join(__dirname, '..', 'packages');
   const packageNames = getDirectories(packagesPath);
 
-  const packagesSummaries = packageNames.reduce((summary, packageName) => {
-    return {
-      ...summary,
-      [packageName]: path.join(packagesPath, packageName, 'coverage', 'coverage-summary.json'),
-    };
-  }, {} as Record<string, string>);
+  const packagesSummaries = packageNames.reduce(
+    (summary, packageName) => {
+      return {
+        ...summary,
+        [packageName]: path.join(
+          packagesPath,
+          packageName,
+          'coverage',
+          'coverage-summary.json'
+        ),
+      };
+    },
+    {} as Record<string, string>
+  );
 
   return { ...appsSummaries, ...packagesSummaries };
 }
@@ -63,7 +79,9 @@ function readSummaryPerPackageAndCreateJoinedSummaryReportWithTotal(
     (summary, packageName) => {
       const reportPath = packagesSummaryPaths[packageName];
       if (fs.existsSync(reportPath)) {
-        const report: { total: CoverageSummary } = JSON.parse(fs.readFileSync(reportPath, 'utf8'));
+        const report: { total: CoverageSummary } = JSON.parse(
+          fs.readFileSync(reportPath, 'utf8')
+        );
 
         const { total } = summary;
 
@@ -72,7 +90,9 @@ function readSummaryPerPackageAndCreateJoinedSummaryReportWithTotal(
             total[key]!.total += report.total[key].total;
             total[key]!.covered += report.total[key].covered;
             total[key]!.skipped += report.total[key].skipped;
-            total[key]!.pct = Number(((total[key]!.covered / total[key]!.total) * 100).toFixed(2));
+            total[key]!.pct = Number(
+              ((total[key]!.covered / total[key]!.total) * 100).toFixed(2)
+            );
           } else {
             total[key] = { ...report.total[key] };
           }
@@ -90,27 +110,26 @@ function readSummaryPerPackageAndCreateJoinedSummaryReportWithTotal(
 function createCoverageReportForVisualRepresentation(
   coverageReport: ConsolidatedCoverageReport
 ): Record<string, Record<string, number>> {
-  return Object.keys(coverageReport).reduce<Record<string, Record<string, number>>>(
-    (report, packageName) => {
-      if (packageName === 'total') return report; // Skip 'total'
+  return Object.keys(coverageReport).reduce<
+    Record<string, Record<string, number>>
+  >((report, packageName) => {
+    if (packageName === 'total') return report; // Skip 'total'
 
-      const packageSummary = coverageReport[packageName];
-      if (!packageSummary) return report; // In case of undefined
+    const packageSummary = coverageReport[packageName];
+    if (!packageSummary) return report; // In case of undefined
 
-      const { lines, statements, functions, branches } = packageSummary;
+    const { lines, statements, functions, branches } = packageSummary;
 
-      return {
-        ...report,
-        [packageName]: {
-          lines: lines?.pct || 0,
-          statements: statements?.pct || 0,
-          functions: functions?.pct || 0,
-          branches: branches?.pct || 0,
-        },
-      };
-    },
-    {}
-  );
+    return {
+      ...report,
+      [packageName]: {
+        lines: lines?.pct || 0,
+        statements: statements?.pct || 0,
+        functions: functions?.pct || 0,
+        branches: branches?.pct || 0,
+      },
+    };
+  }, {});
 }
 
 // Execution Stages
@@ -118,7 +137,9 @@ function createCoverageReportForVisualRepresentation(
 const packagesSummaryPaths = getAllPathsForPackagesSummaries();
 // 2. Generate consolidated report
 const currCoverageReport =
-  readSummaryPerPackageAndCreateJoinedSummaryReportWithTotal(packagesSummaryPaths);
+  readSummaryPerPackageAndCreateJoinedSummaryReportWithTotal(
+    packagesSummaryPaths
+  );
 // 3. Reformat the report for visual representation
 const coverageReportForVisualRepresentation =
   createCoverageReportForVisualRepresentation(currCoverageReport);
