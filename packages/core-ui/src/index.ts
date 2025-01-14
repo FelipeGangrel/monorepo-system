a82c21865af236fd4bcc6e88ffcc796ebb74388e
Felipe Gangrel <felipefrog@gmail.com>
2025-01-14 18:37:46 -0300

    Add Form, Input, and Label UI components with tests
    This commit introduces Form, Input, and Label components, leveraging React Hook Form and Radix UI primitives for enhanced functionality. Unit tests and examples are included to ensure proper rendering, interactivity, and error handling. Additionally, relevant dependencies such as `react-hook-form`, `@testing-library`, and others are added to the project.

--- packages/core-ui/src/index.ts
+++ packages/core-ui/src/index.ts
@@ -4,3 +4,6 @@ export * from './components/ui/alert';
 export * from './components/ui/alert-dialog';
 export * from './components/ui/badge';
 export * from './components/ui/button';
+export * from './components/ui/form';
+export * from './components/ui/input';
+export * from './components/ui/label';
